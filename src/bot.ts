bf809086244e1bbeb070d73beb3b1c6a16e900c6
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-07 17:53:13 -0500

    attempt to publish banshares through the announcement channel

--- src/bot.ts
+++ src/bot.ts
@@ -452,6 +452,11 @@ bot.on("interactionCreate", async (interaction) => {
                     const message = await interaction.message.fetchReference();
                     await message.edit({ components: components(true) });
                     embeds = message.embeds.map((e) => e.toJSON());
+
+                    if (message.crosspostable)
+                        try {
+                            await message.crosspost();
+                        } catch {}
                 } catch {
                     await interaction.editReply({
                         content: "The original banshare could not be found.",
