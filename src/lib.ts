88e0726764790e68eaf4dfb47dff06d36ecd2ca6
AroLeaf <starleafofa@gmail.com>
2023-02-13 13:22:47 +0100

    [feat] remove P3 severity rewrote to the right branch, original by hyper-neutrino

--- src/lib.ts
+++ src/lib.ts
@@ -12,17 +12,7 @@ export function components(published: boolean, severity?: string): any[] {
                         customId: "rescind",
                         label: "Rescind",
                     },
-                    ...(severity === "p3"
-                        ? [
-                              {
-                                  type: ComponentType.Button,
-                                  style: ButtonStyle.Primary,
-                                  customId: "escalate:p2",
-                                  label: "SEV: P2",
-                              },
-                          ]
-                        : []),
-                    ...(severity === "p3" || severity === "p2"
+                    ...(severity === "p2"
                         ? [
                               {
                                   type: ComponentType.Button,
@@ -50,13 +40,6 @@ export function components(published: boolean, severity?: string): any[] {
         {
             type: ComponentType.ActionRow,
             components: [
-                {
-                    type: ComponentType.Button,
-                    style: ButtonStyle.Success,
-                    customId: "sev:p3",
-                    label: "SEV: P3",
-                    disabled: severity === "p3",
-                },
                 {
                     type: ComponentType.Button,
                     style: ButtonStyle.Secondary,
